aac690e0c1af4330c77454b07f8d84001407cc45
YashRajSingh-4799 <64195098+YashRajSingh-4799@users.noreply.github.com>
2021-12-25 20:10:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 (function (window) {
-    var names = ["Aman", "John", "Jyotika", "Jimmy", "Parth", "Saqib", "Aniket", "Sumit", "Ajay", "Yash","Raj","Harjot"];
+    var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
   
     for (var i in names) {
       var firstLetter = names[i].charAt(0).toLowerCase();
